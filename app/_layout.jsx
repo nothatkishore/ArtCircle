cc332bed37d4df3d2c6d85c0abf6629272be1f33
kishoear <kishorear.programming@gmail.com>
2024-06-20 18:01:39 +0530

    Project init

---
+++ app/_layout.jsx
@@ -0,0 +1,18 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+const RootLayout = () =>
+{
+    return(
+
+        <>
+            <Stack>
+                <Stack.Screen name="index" options={{headerShown : false}} />
+            </Stack>
+            <StatusBar style="auto" />
+        </>
+
+    )
+}
+
+export default RootLayout
