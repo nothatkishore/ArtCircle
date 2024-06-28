dbc6d2718a88434e63e5974a97f4852b19e0458c
kishoear <kishorear.programming@gmail.com>
2024-06-28 12:36:19 +0530

    Profile page added

--- app/(profile)/profile.jsx
+++ app/(profile)/profile.jsx
@@ -1,93 +1,105 @@
-import React, { useState } from 'react';
-import { View, Text, Image, TextInput, ScrollView, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Picker } from '@react-native-picker/picker';
-import user from '../../assets/icons/user.png';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import placeHolder from '../../assets/icons/user.png'
+import { Picker } from '@react-native-picker/picker'
+import { StatusBar } from 'expo-status-bar'
 
-const Profile = () => {
-    const [form, setForm] = useState({
-        name: 'Kishore',
-        designation: 'Director',
-        phone: '9840287145',
-        email: 'reachoutkishorear@gmail.com'
-    });
+const profile = () => {
 
-    const [edit, setEdit] = useState(false);
+    const [form, setForm] = useState(
+        {
+            profile: placeHolder,
+            name: 'Stacksnova',
+            email: 'artdirectorsgang@gmail.com',
+            designation: 'Director',
+            contact: '9840287145'
+        })
+
+    const [edit, setEdit] = useState(true)
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
-            <View style={{ margin: 20, alignItems: 'center' }}>
-                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <SafeAreaView className='bg-[#f0f3f6] h-full w-full'>
+
+            <View className='h-1/3 w-full items-center justify-center'>
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                >
                     <Image
-                        source={user}
+                        source={placeHolder}
+                        className='h-24 w-24'
                         resizeMode='contain'
-                        style={{ backgroundColor: 'white', height: 100, width: 100, borderRadius: 50 }}
                     />
-                    <Text style={{ color: 'white', fontSize: 24, marginTop: 10 }}>{form.name}</Text>
-                    <Text style={{ color: 'white', fontSize: 20, marginTop: 5 }}>{form.designation}</Text>
+                </TouchableOpacity>
+                <Text className='mt-2 text-2xl font-light'>{form.name}</Text>
+                <Text className='mt-1 text-lg font-light'>{form.designation}</Text>
+            </View>
+
+            <View className='h-2/3 w-full items-center p-10'>
+
+                {/* Picker */}
+                <View className='bg-slate-50 w-full rounded-lg m-3'>
+                    <Picker
+                        selectedValue={form.designation}
+                        onValueChange={(value) => setForm({ ...form, designation: value })}
+                        enabled={edit}
+                    >
+                        <Picker.Item label="Technician" value="Technician" />
+                        <Picker.Item label="Asst.Director" value="Asst.Director" />
+                        <Picker.Item label="Director" value="Director" />
+                    </Picker>
                 </View>
 
-                <ScrollView style={{ width: '100%' }}>
-                    <View style={{ marginTop: 20, width: '100%' }}>
-                        <Text style={{ color: 'white', fontSize: 12 }}>Name: </Text>
-                        <TextInput
-                            style={{ color: 'white', fontSize: 20, borderBottomColor: 'slategray', borderBottomWidth: 1, marginTop: 5, paddingHorizontal: 5 }}
-                            value={form.name}
-                            editable={edit}
-                            onChangeText={(text) => setForm({ ...form, name: text })}
-                        />
+                {/* Email address */}
+                <View className='border-b border-black w-full rounded-lg m-3'>
+                    <TextInput
+                        placeholder='Email Address'
+                        value={form.email}
+                        onChangeText={(text) => setForm({ ...form, email: text })}
+                        className='p-2'
+                        editable={edit}
+                    />
+                </View>
+
+                <View className='border-b border-black space-y-0 w-full rounded-lg m-3'>
+                    <TextInput
+                        placeholder='Phone number'
+                        value={form.contact}
+                        onChangeText={(text) => setForm({ ...form, contact: text })}
+                        className='p-2'
+                        editable={edit}
+                    />
+                </View>
+
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    className='w-full'
+                    onPress={() => setEdit(!edit)}
+                >
+                    <View className='m-3 rounded-lg bg-[#ffa629]'>
+                        <Text className='p-3 text-white text-base font-medium text-center'>
+                            {edit ? 'Save changes' : 'Edit changes'}
+                        </Text>
                     </View>
-                    <View style={{ marginTop: 20, width: '100%' }}>
-                        <Text style={{ color: 'white', fontSize: 12 }}>Designation: </Text>
-                        {edit ? (
-                            <View style={{ backgroundColor: 'white', borderRadius: 10, marginTop: 5 }}>
-                                <Picker
-                                    selectedValue={form.designation}
-                                    onValueChange={(value) => setForm({ ...form, designation: value })}
-                                >
-                                    <Picker.Item label="Technician" value="Technician" />
-                                    <Picker.Item label="Asst.Director" value="Asst.Director" />
-                                    <Picker.Item label="Director" value="Director" />
-                                </Picker>
-                            </View>
-                        ) : (
-                            <TextInput
-                                style={{ color: 'white', fontSize: 20, borderBottomColor: 'slategray', borderBottomWidth: 1, marginTop: 5, paddingHorizontal: 5 }}
-                                value={form.designation}
-                                editable={false}
-                            />
-                        )}
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    className='w-full'
+                >
+                    <View className='rounded-lg w-full'>
+                        <Text className='p-3 text-blue-600 text-sm font-medium text-center'>
+                            Change Password
+                        </Text>
                     </View>
-                    <View style={{ marginTop: 20, width: '100%' }}>
-                        <Text style={{ color: 'white', fontSize: 12 }}>Phone number: </Text>
-                        <TextInput
-                            style={{ color: 'white', fontSize: 20, borderBottomColor: 'slategray', borderBottomWidth: 1, marginTop: 5, paddingHorizontal: 5 }}
-                            value={form.phone}
-                            editable={edit}
-                            onChangeText={(text) => setForm({ ...form, phone: text })}
-                        />
-                    </View>
-                    <View style={{ marginTop: 20, width: '100%' }}>
-                        <Text style={{ color: 'white', fontSize: 12 }}>Email address: </Text>
-                        <TextInput
-                            style={{ color: 'white', fontSize: 18, borderBottomColor: 'slategray', borderBottomWidth: 1, marginTop: 5, paddingHorizontal: 5 }}
-                            value={form.email}
-                            editable={edit}
-                            onChangeText={(text) => setForm({ ...form, email: text })}
-                        />
-                    </View>
-                    <View style={{ marginVertical: 20, backgroundColor: 'orange', padding: 10, borderRadius: 10 }}>
-                        <TouchableOpacity
-                            activeOpacity={0.7}
-                            onPress={() => setEdit(!edit)}
-                        >
-                            <Text style={{ color: 'white', textAlign: 'center' }}>{edit ? 'Save Changes' : 'Edit Profile'}</Text>
-                        </TouchableOpacity>
-                    </View>
-                </ScrollView>
+                </TouchableOpacity>
+
             </View>
+
+            <StatusBar style='light' backgroundColor='#ffa629' />
+
         </SafeAreaView>
-    );
+    )
 }
 
-export default Profile;
+export default profile
