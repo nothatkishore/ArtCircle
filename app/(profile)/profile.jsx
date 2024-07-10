95d96fe654ae28ae1c84766e8fe2e3429a2ff9d0
kishoear <kishorear.programming@gmail.com>
2024-07-10 12:30:42 +0530

    Profile card added

--- app/(profile)/profile.jsx
+++ app/(profile)/profile.jsx
@@ -1,167 +1,104 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import placeHolder from '../../assets/icons/user.png'
-import { Picker } from '@react-native-picker/picker'
+import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import * as ImagePicker from 'expo-image-picker'
-import { useSelector, useDispatch } from 'react-redux'
-import { setProfile, setName, setDesignation } from '../../redux/slices/userSlice'
-import { router } from 'expo-router'
+import profilePic from '../../assets/icons/user.png'
 
 const profile = () => {
 
-    const profile = useSelector((state) => state.user.profile)
-    const dispatch = useDispatch()
+    const [form, setform] = useState({
+        profile: null,
+        username: 'Stacksnova',
+        designation: 'Art director',
+        contact: '',
+        econtact: '',
+        email: '',
+        pAddress: '',
+        rAddress: ''
+    })
 
-    const logout = () =>
-    {
-        router.dismissAll()
-        router.replace('/signIn')
-    }
-
-    const handlePress = async () => {
-        if (!edit)
-            return
-
-        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
-        if (permissionResult.granted === false) {
-            alert('Permission to access gallery is required!')
-            return
-        }
-
-        const result = await ImagePicker.launchImageLibraryAsync(
-            {
-                mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                allowsEditing: true,
-                aspect: [4, 3],
-                quality: 1,
-            })
-
-        if (!result.canceled) {
-            dispatch(setProfile(result.assets[0].uri))
-        }
-    }
-
-    //Make a request to get these details
-    const [form, setForm] = useState(
-        {
-            profile: placeHolder,
-            name: 'Stacksnova',
-            email: 'artdirectorsgang@gmail.com',
-            designation: 'Director',
-            contact: '9840287145'
-        })
-
-    useEffect(() => {
-        dispatch(setName(form.name))
-        dispatch(setDesignation(form.designation))
-    }, [form])
-
-
-    const [edit, setEdit] = useState(false)
+    const [edit, setedit] = useState(false)
 
     return (
-        <SafeAreaView className='bg-[#f0f3f6] h-full w-full'>
+        <SafeAreaView className='flex-1 bg-slate-100'>
+            <ScrollView className='px-5 py-7'>
 
-            <View className='h-1/3 w-full items-center justify-center'>
-                <TouchableOpacity
-                    activeOpacity={edit ? 0.8 : 1}
-                    onPress={handlePress}
-                >
-                    <View className={edit && 'bg-white p-2 rounded-full'}>
+                <View className='bg-white p-3 mb-7 rounded-lg shadow-2xl'>
+                    <Text className='text-lg text-center text-gray-700'>Member profile</Text>
+                    {
+                        edit &&
+                        <TouchableOpacity
+                            activeOpacity={0.7}
+                            onPress={() => setedit(false)}
+                        >
+                            <View className='bg-green-500 p-2 rounded-lg mt-2'>
+                                <Text className='text-white text-center'>Save changes</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
+                </View>
+
+                <View className='bg-white p-7 flex-row justify-around rounded-lg'>
+
+                    {/* Profile picture */}
+                    <View className='w-auto p-2 justify-center items-center'>
                         <Image
-                            source={profile ? { uri: profile } : placeHolder}
-                            className='h-24 w-24 rounded-full '
-                            resizeMode='cover'
+                            source={profilePic}
+                            className='h-24 w-24'
                         />
+                        <Text className='text-lg'>{form.username}</Text>
+                        <Text className='text-sm'>{form.designation}</Text>
+                        {
+                            edit &&
+                            <>
+                                <TouchableOpacity
+                                    activeOpacity={0.7}
+                                >
+                                    <View className='bg-slate-200 p-2 rounded-lg mt-2'>
+                                        <Text className='text-slate-700'>Change photo</Text>
+                                    </View>
+                                </TouchableOpacity>
 
-                        {edit && <Text className='text-center'>Change</Text>}
 
-                    </View>
-                </TouchableOpacity>
-                <Text className='mt-2 text-2xl font-light'>{form.name}</Text>
-                <Text className='mt-1 text-lg font-light'>{form.designation}</Text>
-            </View>
-
-            <View className='h-2/3 w-full items-center p-10'>
-
-                {/* Picker */}
-                <View className='bg-slate-50 w-full rounded-lg m-3'>
-                    <Picker
-                        selectedValue={form.designation}
-                        onValueChange={(text) => setForm({ ...form, designation: text })
+                            </>
                         }
-                        enabled={edit}
-                    >
-                        <Picker.Item label="Technician" value="Technician" />
-                        <Picker.Item label="Asst.Director" value="Asst.Director" />
-                        <Picker.Item label="Director" value="Director" />
-                    </Picker>
+                    </View>
+
+                    {/* Side buttons */}
+                    {
+                        !edit &&
+                        <View className='p-2 justify-center gap-2'>
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                                onPress={() => setedit(true)}
+                            >
+                                <View className='bg-blue-500 py-3 rounded-xl'>
+                                    <Text className='text-white text-xs text-center'>Edit</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                            >
+                                <View className='bg-green-500 py-3 px-3 rounded-xl'>
+                                    <Text className='text-white text-xs text-center'>Change password</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                activeOpacity={0.7}
+                            >
+                                <View className='bg-red-500 py-3 px-3 rounded-xl text-center'>
+                                    <Text className='text-white text-xs text-center'>Logout</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    }
+
                 </View>
 
-                {/* Email address */}
-                <View className='border-b border-black w-full rounded-lg m-3'>
-                    <TextInput
-                        placeholder='Email Address'
-                        value={form.email}
-                        onChangeText={(text) => setForm({ ...form, email: text })}
-                        className='p-2'
-                        editable={edit}
-                    />
-                </View>
 
-                <View className='border-b border-black space-y-0 w-full rounded-lg m-3'>
-                    <TextInput
-                        placeholder='Phone number'
-                        value={form.contact}
-                        onChangeText={(text) => setForm({ ...form, contact: text })}
-                        className='p-2'
-                        editable={edit}
-                    />
-                </View>
-
-                <TouchableOpacity
-                    activeOpacity={0.8}
-                    className='w-full'
-                    onPress={() => setEdit(!edit)}
-                >
-                    <View className='m-3 rounded-lg bg-[#ffa629]'>
-                        <Text className='p-3 text-white text-base font-medium text-center'>
-                            {edit ? 'Save changes' : 'Edit profile'}
-                        </Text>
-                    </View>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    activeOpacity={0.8}
-                    className='w-full'
-                    onPress={() => router.push('/CreatePass')}
-                >
-                    <View className='rounded-lg w-full'>
-                        <Text className='p-3 text-blue-600 text-sm font-medium text-center'>
-                            Change Password
-                        </Text>
-                    </View>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    activeOpacity={0.7}
-                    className='w-full mt-5'
-                    // onPress={() => router.canGoBack}
-                    onPress={logout}
-                >
-                    <View className='rounded-lg w-full bg-red-500'>
-                        <Text className='p-2 text-white text-base font-medium text-center'>
-                            Logout
-                        </Text>
-                    </View>
-                </TouchableOpacity>
-
-            </View>
-
+            </ScrollView>
             <StatusBar style='light' backgroundColor='#ffa629' />
-
         </SafeAreaView>
     )
 }
